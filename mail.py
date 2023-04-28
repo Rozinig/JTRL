201183689359c51e16074fcd7980b8c495ea8c9b
Rozinig <32249995+Rozinig@users.noreply.github.com>
2023-04-27 20:19:51 -0700

    Forget the actually email code'

---
+++ mail.py
@@ -0,0 +1,21 @@
+import smtplib, ssl
+from email.message import EmailMessage
+
+def contactemail(email, name, usermessage, cred):
+    print(cred['email'], cred['server'], cred['port'], cred['pass'])
+    message = EmailMessage()
+    message["Subject"] = f"{name} sent you a message"
+    message["From"] = cred['email']
+    message["To"] = cred['email']
+    message['reply-to'] = email
+
+    body = f"""User, {name} with the {email} has sent you a message through the contact form on JTRL: {usermessage}"""
+    message
+
+    # Create secure connection with server and send email
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(cred['server'], cred['port'], context=context) as server:
+        server.login(cred['email'], cred['pass'])
+        server.sendmail(
+            cred['email'], cred['email'], message.as_string()
+        )
